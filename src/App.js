f3c8544c1e03f47c5080b60cbf3e246a46bb22b1
wnsdud96908 <wnsdud96908@naver.com>
2023-08-11 18:48:06 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import Layout from "./Layout/Layout";
+import Display from "./components/Display";
+import Slidebar from "./components/SlideBar";
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <Slidebar />
+      <Display />
+    </Layout>
   );
 }
-
-export default App;
